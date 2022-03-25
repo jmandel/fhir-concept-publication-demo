8c4ea1bff594c62bcd674c06b2ac124f996705d3
Josh Mandel <jmandel@med.harvard.edu>
2022-03-25 09:21:50 -0500

    s/ipfs/github/g <g>

--- scripts/rxnorm-publish.sh
+++ scripts/rxnorm-publish.sh
@@ -3,10 +3,11 @@ NAME=${NAME:-rxnorm}
 VERSION=${VERSION:-03072022}
 FILENAME="CodeSystem-${NAME}-${VERSION}.ndjson.gz"
 
-NDJSON_CID=$(ipfs add -q ${FILENAME})
-echo ".ndjzon CID $NDJSON_CID"
+# NDJSON_CID=$(ipfs add -q ${FILENAME})
+# echo ".ndjzon CID $NDJSON_CID"
 
-jq ". + {extension: [{url: \"https://tx.fhir.me/concepts-as-ndjson-gz\", valueUrl: \"ipfs://${NDJSON_CID}\"}]}" \
+HOSTED_FILE_REF="https://raw.githubusercontent.com/jmandel/fhir-concept-publication-demo/main/${FILENAME}"
+jq ". + {extension: [{url: \"https://tx.fhir.me/concepts-as-ndjson-gz\", valueUrl: \"${HOSTED_FILE_REF}\"}]}" \
 <(zcat ${FILENAME}| head -n 1) \
 > CodeSystem-${NAME}-${VERSION}-with-concept-link.json 
 
@@ -15,6 +16,6 @@ jq '. + {concepts: $inputs} | del(.extension)' \
 <(zcat ${FILENAME} | head -n 1) |
 gzip  > CodeSystem-${NAME}-${VERSION}.json.gz
 
-CID=$( ipfs add -q -r . | tail -n 1 )
-echo "Added ipfs://${CID}"
-echo "Browse at https://explore.ipld.io/#/explore/${CID}"
+# CID=$( ipfs add -q -r . | tail -n 1 )
+# echo "Added ipfs://${CID}"
+# echo "Browse at https://explore.ipld.io/#/explore/${CID}"
